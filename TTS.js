724db9f06d1e030ee518503d14ce0bb2d56217ea
João Costa <jdpc557@gmail.com>
2013-06-26 20:08:13 +0100

    Adicionado CSS temporário e bugfixes de compatibilidade (HTML5 e possibilidade de alterar o CSS mais facilmente)

--- TTS.js
+++ TTS.js
@@ -1,4 +1,17 @@
-var baseURL="/~ei09082/TTC/Alpha"; //versão para o ni: "/TTS"
+var baseURL="/timetableselector"; //versão para o ni: "/TTS"
+
+// Campos do horário
+var diasSemana=new Array("Segunda","Terça","Quarta","Quinta","Sexta","Sábado");
+var diasSemanaMin=new Array("seg","ter","qua","qui","sex","sab");
+var horas=new Array(
+	"8:00 - 8:30","8:30 - 9:00","9:00 - 9:30","9:30 - 10:00","10:00 - 10:30","10:30 - 11:00",
+	"11:00 - 11:30","11:30 - 12:00","12:00 - 12:30","12:30 - 13:00","13:00 - 13:30","13:30 - 14:00",
+	"14:00 - 14:30","14:30 - 15:00","15:00 - 15:30","15:30 - 16:00","16:00 - 16:30","16:30 - 17:00",
+	"17:00 - 17:30","17:30 - 18:00","18:00 - 18:30","18:30 - 19:00","19:00 - 19:30","19:30 - 20:00",
+	"20:00 - 20:30");
+var horasMin=new Array(
+	"8","8-5","9","9-5","10","10-5","11","11-5","12","12-5","13","13-5","14","14-5","15","15-5",
+	"16","16-5","17","17-5","18","18-5","19","19-5","20","20-5");
 
 function Aula(jsonobj){
 	this.dia=Number(jsonobj.dia)+1;
@@ -23,9 +36,16 @@ function Aula(jsonobj){
 	if (this.tipo=="PL") this.tipoh="praticalaboratorial";
 	
 	
-	this.stleft=100*this.dia-68;
+	/*this.stleft=100*this.dia-68;
 	this.sttop=23+23*this.horarow;
-	this.stheight=23*this.duracao-9;
+	this.stheight=23*this.duracao-9;*/
+	var diaString=diasSemanaMin[this.dia-1];
+	var horaString=horasMin[this.horarow-1];
+	var bloco=$("#horario"+diaString+horaString);
+	this.stleft=bloco.offset().left-1;
+	this.sttop=bloco.offset().top-1;
+	this.stwidth=bloco.outerWidth()-1;
+	this.stheight=bloco.outerHeight()*this.duracao-1;
 	
 }
 
@@ -56,7 +76,8 @@ function Cadeira(sigla,jsonobj){
 Cadeira.prototype.selectorhtml=function(){
 	var str='';
 	str+='<div class=classselector>';
-	str+='<p>'+this.nomec+' ('+this.nome+')'+'</p>';
+	//str+='<p>'+this.nomec+' ('+this.nome+')'+'</p>';
+	str+=this.nomec+' ('+this.nome+')';
 	str+='<select class="turmaselect" data-cadeira="'+this.nome+'">';
 	str+='<option value="-">-----</option>';
 	for (var i=0;i<this.praticas.length;i++)
@@ -78,21 +99,21 @@ Cadeira.prototype.showTurma=function(){
 			$.each(this.teoricas,function(i,aula){
 				if(aula.turma==turmaselect||turmaselect=="teoricas")
 				{
-					$('#horariotable').append(aula.horariohtml());
+					$('#content').append(aula.horariohtml());
 				}
 			});
 		}
 		$.each(this.praticas,function(i,aula){
 			if(aula.turma==turmaselect)
 			{
-				$('#horariotable').append(aula.horariohtml());
+				$('#content').append(aula.horariohtml());
 			}
 		});
 	}
 }
 Aula.prototype.horariohtml=function(){
 	var str='';
-	str+='<div class="'+this.tipoh+' aula" data-cadeira="'+this.cadeira+'" style="left:'+this.stleft+';top:'+this.sttop+';height:'+this.stheight+';"><div class="aulawrapper">';
+	str+='<div class="'+this.tipoh+' aula" data-cadeira="'+this.cadeira+'" style="left:'+this.stleft+'px;top:'+this.sttop+'px;height:'+this.stheight+'px;width:'+this.stwidth+'px;"><div class="aulawrapper">';
 	str+='<p class="aula"><span class="aula"><abbr title="'+this.cadeiran+'">'+this.cadeira+'</abbr></span><span class="aula"><abbr title="'+this.prof+'">'+this.profsig+'</abbr></span></p>';
 	str+='<p class="aula"><span class="aula">'+this.sala+'</span><span class="aula">'+this.turmac+'</span></p>';
 	str+='</div></div>';
@@ -107,6 +128,8 @@ var curso;
 var cadeiras;
 
 $(document).ready(function(){
+	generateTimetable();
+
 	$.blockUI.defaults.css = {};
 	$.blockUI({ message: $('#promptcurso') }); 
 
@@ -185,5 +208,26 @@ function addCadeiras(){
 	$('input.listcad:checked').each(function(index,element){
 		$('#selectorsdiv').append(cadeiras[$(element).val()].selectorhtml());
 	});
+}
+
+// Cria a tabela o horário
+function generateTimetable() {
+	var linha="";
+	linha+="<tr><th>Horas</th>";
+	for (d in diasSemana) {
+		linha+="<th>"+diasSemana[d]+"</th>";
+	}
+	linha+="</tr>";
+	$("#horario").append(linha);
+
+	for (h in horas) {
+		linha="<tr><th>"+horas[h]+"</th>";
+		for (d in diasSemana) {
+			linha+="<td id='horario"+diasSemanaMin[d]+horasMin[h]+"'></td>"
+		}
+		linha+="</tr>";
+		$("#horario").append(linha);
+	}
+
 }
 
